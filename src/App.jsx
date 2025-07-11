40f3392890ecb455a245654ae67152d1d0394beb
luislucasinter14@hotmail.com <luislucasinter14@hotmail.com>
2025-07-11 11:28:09 -0300

    Estrutura Inicial

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Componentes/Header';
+import Footer from './Componentes/Footer';
+import Home from './Componentes/Home';
+import Login from './Componentes/Login/Login';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;

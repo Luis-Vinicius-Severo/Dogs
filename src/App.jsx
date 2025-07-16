985013a49d017614ad66609ba73902e2d1b84f9c
luislucasinter14@hotmail.com <luislucasinter14@hotmail.com>
2025-07-16 12:17:04 -0300

    userContext para o usuario

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Header from './Componentes/Header';
 import Footer from './Componentes/Footer';
 import Home from './Componentes/Home';
 import Login from './Componentes/Login/Login';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );

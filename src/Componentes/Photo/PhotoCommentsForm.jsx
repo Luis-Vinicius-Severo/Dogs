b7c2472bd29caac9cc041360195f5f15743fadca
luislucasinter14@hotmail.com <luislucasinter14@hotmail.com>
2025-08-04 13:16:30 -0300

    Comentário Estilo

--- src/Componentes/Photo/PhotoCommentsForm.jsx
+++ src/Componentes/Photo/PhotoCommentsForm.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Enviar from '../../Assets/enviar.svg?react';
 import useFetch from '../../Hooks/useFetch';
 import { COMMENT_POST } from '../../api';
+import styles from './PhotoCommentsForm.module.css';
+import Error from '../Helper/Error';
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
@@ -18,15 +20,16 @@ const PhotoCommentsForm = ({ id, setComments }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
       <Error error={error} />
